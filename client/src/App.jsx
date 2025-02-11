4acf1e78b186bbcb2d74f71d9de58cf5de46621a
islamgues <islam.gues@yahoo.fr>
2025-02-11 14:51:12 +0100

    Added client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-[36px]">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
